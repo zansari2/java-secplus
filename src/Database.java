81848c6c441d09f631d34f3b063942839ac99dc4
Zishan Ansari <42755217+zansari2@users.noreply.github.com>
2019-07-31 12:01:44 -0400

    Update Database.java
    Simplified imports, added new diagnostic tools

--- src/Database.java
+++ src/Database.java
@@ -1,9 +1,5 @@
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.nio.file.Path;
-import java.io.File;
+import java.io.*;
 
 /**
  * A set of questions
@@ -32,8 +28,16 @@ public class Database
             reader.read(a);
             for(char c : a)
             {
-                System.out.println(c);
+                if(c!='\n')
+                {
+                    System.out.print(c);
+                }
+                else
+                {
+                    System.out.println();
+                }
             }
+            System.out.println();
             reader.close();
             
         }
@@ -43,9 +47,71 @@ public class Database
         }
         
     }
+    /**
+     * Runs a file diagnostic
+     * @param path the path
+     */
+    public void diag(Path path) 
+    { 
+        try
+        {
+            File file = new File(path.toString()); 
+            FileInputStream fileStream = new FileInputStream(file); 
+            InputStreamReader input = new InputStreamReader(fileStream); 
+            BufferedReader reader = new BufferedReader(input); 
+            
+            String line; 
+            
+            // Initializing counters 
+            int countWord = 0; 
+            int sentenceCount = 0; 
+            int characterCount = 0; 
+            int paragraphCount = 1; 
+            int whitespaceCount = 0; 
+            
+            // Reading line by line from the  
+            // file until a null is returned 
+            while((line = reader.readLine()) != null) 
+            { 
+                if(line.equals("")) 
+                { 
+                    paragraphCount++; 
+                } 
+                if(!(line.equals(""))) 
+                { 
+                    
+                    characterCount += line.length(); 
+                    
+                    // \\s+ is the space delimiter in java 
+                    String[] wordList = line.split("\\s+"); 
+                    
+                    countWord += wordList.length; 
+                    whitespaceCount += countWord -1; 
+                    
+                    // [!?.:]+ is the sentence delimiter in java 
+                    String[] sentenceList = line.split("[!?.:]+"); 
+                    
+                    sentenceCount += sentenceList.length; 
+                } 
+            } 
+            
+            reader.close();
+
+            System.out.println("Total word count = " + countWord); 
+            System.out.println("Total number of sentences = " + sentenceCount); 
+            System.out.println("Total number of characters = " + characterCount); 
+            System.out.println("Number of paragraphs = " + paragraphCount); 
+            System.out.println("Total number of whitespaces = " + whitespaceCount); 
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    } 
     public static void main(String[] args) 
     {
         Path path = Path.of("C:/Users/zansari/Documents/GitHub/java-secplus/Hello.txt");
         Database test = new Database(path);
+        test.diag(path);
     }
 }
