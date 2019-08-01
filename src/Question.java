2a657b758dc16d68fe78f5fb618b5264f1b5f016
Zishan Ansari <42755217+zansari2@users.noreply.github.com>
2019-08-01 11:29:16 -0400

    Update Question.java

--- src/Question.java
+++ src/Question.java
@@ -1,4 +1,19 @@
+import java.nio.file.Path;
 public class Question
 {
-    
+    String[] prompt;
+    String[] answer;
+
+    public Question()
+    {
+        Path path = Path.of("C:/Users/zansari/Documents/GitHub/java-secplus/Data.txt");
+        Database data = new Database(path);
+        prompt = data.getDefinitions();
+        answer = data.getTerms();
+    }
+
+    public static void main(String[] args) 
+    {
+        
+    }
 }
